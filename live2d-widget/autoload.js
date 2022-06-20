bbdf9d00763600616c463b4002ff2b5881903a6d
GuuGuai <15389176616@163.com>
2022-06-20 22:50:33 +0800

    Site updated: 2022-06-20 22:50:32

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
+			apiPath: "https://121.4.132.153/live2d-API/",
+			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
 }
